bd2516ea039f8d130fbee9366c458c157aa4084f
neveldo <neveldo@gmail.com>
2016-04-17 23:56:37 +0200

    Remove empty arguments and arguments that contain empty tags

--- src/TextGenerator/TextFunction/ShuffleFunction.php
+++ src/TextGenerator/TextFunction/ShuffleFunction.php
@@ -6,7 +6,7 @@ use Neveldo\TextGenerator\Tag\TagReplacerInterface;
 
 /**
  * Class ShuffleFunction
- * Parser for 'shuffle' function :  returns the parameters shuffled.
+ * 'shuffle' function :  returns the parameters shuffled.
  * The first parameter is the separator between each others.
  * Examples :
  * #shuffle{ |one|two|three}
@@ -38,13 +38,18 @@ class ShuffleFunction implements FunctionInterface
     {
         if (count($arguments) < 2) {
             Throw new \InvalidArgumentException(
-                sprintf("ShuffleParser expect at least two parameters, %d given.", count($arguments))
+                sprintf("ShuffleFunction expect at least two parameters, %d given.", count($arguments))
             );
         }
 
         $separator = array_shift($arguments);
 
-        $arguments = array_filter(array_map("trim", $arguments));
+        $arguments = array_map("trim", $arguments);
+
+        // Remove empty arguments and arguments that contain empty tags
+        $arguments = array_filter($arguments, function($item) {
+           return  ($item !== '' && strpos($item, $this->tagReplacer->getEmptyTag()) === false);
+        });
 
         shuffle($arguments);
 
