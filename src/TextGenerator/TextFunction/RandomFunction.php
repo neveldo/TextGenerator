29ce8001646f96ade9cda1057dfa5fe32ec4948a
neveldo <neveldo@gmail.com>
2016-07-31 22:26:26 +0200

    Fix RandomFunction

--- src/TextGenerator/TextFunction/RandomFunction.php
+++ src/TextGenerator/TextFunction/RandomFunction.php
@@ -37,7 +37,7 @@ class RandomFunction implements FunctionInterface
     {
         // Remove empty arguments and arguments that contain empty tags
         $arguments = array_filter($arguments, function($item) {
-            return  ($item !== '' && strpos($item, $this->tagReplacer->getEmptyTag()) === false);
+            return  (strpos($item, $this->tagReplacer->getEmptyTag()) === false);
         });
 
         if (count($arguments) === 0) {
