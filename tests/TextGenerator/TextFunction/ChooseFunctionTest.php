edfaffb08d5fed889357ec89ae99af1df8f9250b
neveldo <neveldo@gmail.com>
2016-08-03 13:58:17 +0200

    Add unit tests for ChooseFunction

---
+++ tests/TextGenerator/TextFunction/ChooseFunctionTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Neveldo\TextGenerator\Tag;
+
+use Neveldo\TextGenerator\TextFunction\ChooseFunction;
+
+class ChooseFunctionTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp() {
+        $this->tagReplacer = new TagReplacer();
+        $this->function = new ChooseFunction($this->tagReplacer);
+    }
+
+    public function testSimpleChoose()
+    {
+        $result = $this->function->execute([2, 'test1','test2', 'test3']);
+        $this->assertEquals('test2', $result);
+    }
+
+    public function testSimpleChoose2()
+    {
+        $result = $this->function->execute(['3', 'test1','test2', 'test3']);
+        $this->assertEquals('test3', $result);
+    }
+
+    public function testChooseWithUnexistantArg()
+    {
+        $result = $this->function->execute([0, 'test1','test2', 'test3']);
+        $this->assertEquals('', $result);
+    }
+
+    public function testChooseWithUnexistantArg2()
+    {
+        $result = $this->function->execute([7, 'test1','test2', 'test3']);
+        $this->assertEquals('', $result);
+    }
+}
