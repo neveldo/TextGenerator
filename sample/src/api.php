3f03dd00f0ab585f88a0f36210fdb5424a26412b
neveldo <neveldo@gmail.com>
2016-08-17 22:43:35 +0200

    Add sample API

---
+++ sample/src/api.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Simple API using TextGenerator
+ * HTTP post parameters :
+ *   string template : The template to use to generate the texts
+ *   array data :
+ * [
+ *   {'tag1': 'value', 'tag2': 'value'},
+ *   {'tag1': 'value', 'tag2': 'value'},
+ *   ...
+ * ]
+ *
+ * Return following JSON object :
+ * {
+ *   'result': [
+ *     'text 1',
+ *     'text 1',
+ *     ...,
+ *   ],
+ *   'error': ''
+ * }
+ */
+
+if (!file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+    echo "Please run 'composer install' on the root directory before running the sample script.";
+    return;
+}
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+use Neveldo\TextGenerator\TextGenerator;
+
+$output = [
+    'result' => '',
+    'error' => '',
+];
+
+if (!isset($_POST['template']) || !isset($_POST['data'])) {
+    $output['error'] = 'Missing POST attribute "template" or "data".';
+}
+
+if ($output['error'] === '') {
+    $textGenerator = new TextGenerator();
+    $textGenerator->compile($_POST['template']);
+
+    try {
+        $data = json_decode($_POST['data'], true);
+
+        foreach($data as $row) {
+            $output['result'][] = $textGenerator->generate($row);
+        }
+
+    } catch (\Exception $e) {
+        $output['error'] = $e->getMessage();
+    }
+}
+
+echo json_encode($output);
